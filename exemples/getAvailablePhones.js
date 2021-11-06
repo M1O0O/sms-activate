1671a3f65291d8d1777e6ba63d37cf4d37252fa7
M1000 <formentel.remi@gmail.com>
2021-11-06 01:21:58 +0100

    Add getAvailablePhones

---
+++ exemples/getAvailablePhones.js
@@ -0,0 +1,7 @@
+const api = require('sms-activate.ru');
+
+(async function () {
+    const me = new api("My_Api_Key");
+    const phone = await me.getAvailablePhones(["megafon", "mts", "beeline", "tele2"], 0);
+    console.log(phone);
+})()
